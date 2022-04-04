a8f3a6a96c3569a03c6bb09dd086b8b4e01f1e0e
Lucianaszwimer <91558510+Lucianaszwimer@users.noreply.github.com>
2022-04-04 10:49:03 -0300

    Add files via upload

---
+++ Ejercicios/2.js
@@ -0,0 +1,11 @@
+const data = [2, 3, 5, 12, 54, 5, -1, 0, 4, 23, 66, 7];
+
+const data2 = [];
+var aux = 0;
+    for(var i=0; i<data.length; i++)
+    {
+        aux=data2[i]
+        data2[1]=data[i]*data[i-1]-data[i+1];
+        console.log(data2);
+    }
+
